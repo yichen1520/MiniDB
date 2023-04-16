33da9b8c4662e48a445797d0b7c4019b73217b7f
wangteng <wt10051520@163.com>
2023-04-16 22:31:51 +0800

    解决无法正常连接问题2

--- src/main/java/com/WangTeng/MiniDB/engine/net/handler/factory/FrontConnectionFactory.java
+++ src/main/java/com/WangTeng/MiniDB/engine/net/handler/factory/FrontConnectionFactory.java
@@ -1,6 +1,9 @@
 package com.WangTeng.MiniDB.engine.net.handler.factory;
 
+import com.WangTeng.MiniDB.config.SystemConfig;
 import com.WangTeng.MiniDB.engine.net.handler.fronted.FrontendConnection;
+import com.WangTeng.MiniDB.engine.net.handler.fronted.ServerQueryHandler;
+import com.WangTeng.MiniDB.engine.session.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +17,13 @@ public class FrontConnectionFactory {
 
     public FrontendConnection getConnection(){
         FrontendConnection connection = new FrontendConnection();
-
+        connection.setQueryHandler(new ServerQueryHandler(connection));
+        connection.setId(ACCEPT_SEQ.getAndIncrement());
+        logger.info("connection Id=" + connection.getId());
+        connection.setCharset(SystemConfig.DEFAULT_CHARSET);
+        connection.setTxIsolation(SystemConfig.DEFAULT_TX_ISOLATION);
+        connection.setLastActiveTime();
+        connection.setSession(SessionFactory.newSession(connection));
         return connection;
     }
 }
